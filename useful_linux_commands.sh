c3a7733e7e72ab6d5fb738822c8105a7d1340ddf
Mohammad Shadab <120562941+Imshad18@users.noreply.github.com>
2025-02-10 13:51:24 +0530

    Update useful_linux_commands.sh

--- useful_linux_commands.sh
+++ useful_linux_commands.sh
@@ -23,11 +23,16 @@ cat file.txt | awk 'NR < 11 {print $0}'   : print lines above line 11
 cat file.txt | awk 'NR == 11 {print $0}'  : print line 11
 
 
-
 sort | uniq    : removes duplicate
 sort | uniq -c : removes duplicate & counts number of occurance of each value
 sort -nr   : search numerically and recursively
-
+rev : reverse strig characters
+cut -f 1 : cut field 1
+cut -d '.' -f 1-2  : split the string on every dot and keep the first 2 fields
+grep -v 'test' : display lines that dont match the word test
+grep -v -e 'test1' -e 'test2'  :  display lines that don't match one or both "test1" and "test2" strings.
+file  : view file info
+grep -rin Testvalue1 * | column -t | less -S  : search the "Testvalue1" string everywhere, organise column spaces and view the output with less.
 
 
 
